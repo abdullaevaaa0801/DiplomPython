66644c4e8b91a67dadf332146f4832fcbc4dc8c6
abdullaevaaa0801 <abdullaevaaa0801@gmail.com>
2024-10-11 14:10:05 +0300

    Diplom2.0

--- Pages/page_ui.py
+++ Pages/page_ui.py
@@ -1,10 +1,5 @@
-import pytest
-from selenium import webdriver
+import allure
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from tests.constants import Kinopoisk_base_url
 
 
 class BasePage:
@@ -12,7 +7,40 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
+    @allure.step("Поиск в шапке страницы в поле поиск")
     def search(self, params, id, result):
         self.driver.find_element(By.NAME, "kp_query").send_keys(params)
         self.driver.find_element(By.ID, id).click()
         return self.driver.find_element(By.CSS_SELECTOR, result).text
+
+    @allure.step("Поиск Топ-250 фильмов")
+    def top_250(self):
+        self.driver.find_element(By.CSS_SELECTOR, '[aria-label="Расширенный поиск"]').click()
+        self.driver.find_element(By.CSS_SELECTOR, 'a[href="/level/20/"').click()
+
+    @allure.step("Покупка билетов в кино")
+    def buy_ticket(self):
+        self.driver.find_element(By.CSS_SELECTOR, 'a[class="styles_link__KtvyW"]').click()
+        self.driver.find_element(By.CSS_SELECTOR, 'a[class="styles_root__omMgy styles_item__pq3A4"]').click()
+        self.driver.find_element(By.CSS_SELECTOR, 'a[href="/afisha/new/film/5452393/"]').click()
+        self.driver.find_element(By.CSS_SELECTOR, 'span[class="calendar-filter__title-day"]').click()
+        self.driver.find_element(By.CSS_SELECTOR, 'div[aria-label="day-20"]').click()
+        self.driver.find_element(By.CSS_SELECTOR, 'span[data-template="schedule-item__template"]').click()
+
+    @allure.step("Проскролить страницу до нужного элемента")
+    def scroll(self):
+        self.driver.execute_script("window.scrollBy(0,800)")
+
+    @allure.step("Поиск сериалов")
+    def series(self):
+        self.driver.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/nav[1]/ul[1]/li[4]/a[1]").click()
+        self.driver.find_element(By.CSS_SELECTOR, 'a[href="/lists/categories/movies/8/"]').click()
+        self.driver.find_element(By.CSS_SELECTOR, 'img[src="//avatars.mds.yandex.net/get-bunker/50064/546ba464afc21764e58be3987df5063a0a2f9da9/192x192"]').click()
+        self.driver.find_element(By.CSS_SELECTOR, 'a[href="/series/502838/"]').click()
+
+    @allure.step("Поиск селектора поддержки")
+    def support_service(self):
+        self.driver.find_element(By.XPATH, "//button[@class='styles_contentButton__Yfvdh']").click()
+
+        """ Использовать при первом запуске теста """
+        # # self.driver.find_element(By.CSS_SELECTOR, 'button[data-test-tag="confirm-dialog-ok"]').click()
---
+++ tests/test_UI.py
@@ -0,0 +1,75 @@
+from time import sleep
+
+import allure
+import pytest
+from selenium import webdriver
+from Pages.page_ui import BasePage
+
+
+@pytest.fixture()
+def chrome_browser():
+    driver = webdriver.Chrome()
+    driver.maximize_window() 
+    driver.implicitly_wait(100)
+    yield driver
+    driver.quit()
+
+@allure.id("Тест-1")
+@allure.severity ("Blocker")
+@allure.title("Поиск актера")
+@allure.description("Поиск актера 'Брэд Питт' в шапке страницы в поле поиск")
+def test_search_actor(chrome_browser):
+    with allure.step("Открытие главной страницы"):
+        chrome_browser.get("https://www.kinopoisk.ru")
+        page = BasePage(chrome_browser)
+    with allure.step("Проверка поиск в шапке страницы актера"):
+        assert page.search("Брэд Питт", "suggest-item-person-25584", 'h1[data-tid="f22e0093"]') == "Брэд Питт"
+
+@allure.id("Тест-2")
+@allure.severity ("Critical")
+@allure.title("Переход в Топ-250 фильмов")
+@allure.description("Переход на страницу с результатами поиска Топ-250 фильмов")
+def test_top_250(chrome_browser):
+    with allure.step("Открытие главной страницы"):
+        chrome_browser.get("https://www.kinopoisk.ru")
+        page = BasePage(chrome_browser)
+    with allure.step("Переход на страницу Топ-250 фильмов"):
+        page.top_250()
+
+@allure.id("Тест-3")
+@allure.severity ("Critical")
+@allure.title("Покупка билетов в кино")
+@allure.description("Переход на страницу для покупки билетов в кино")
+def test_buy_ticket(chrome_browser):
+    with allure.step("Открытие главной страницы"):
+        chrome_browser.get("https://www.kinopoisk.ru")
+        page = BasePage(chrome_browser)
+    with allure.step("Проскролить страницу до нужного элемента"):
+        page.scroll()
+    with allure.step("Переход на страницу для покупки билетов в кино"):
+        page.buy_ticket()
+
+@allure.id("Тест-4")
+@allure.severity ("Critical")
+@allure.title("Выбор сериала")
+@allure.description("Переход на страницу с сериалами")
+def test_series(chrome_browser):
+    with allure.step("Открытие главной страницы"):
+        chrome_browser.get("https://www.kinopoisk.ru")
+        page = BasePage(chrome_browser)
+    with allure.step("Переход на страницу с сериалами"):
+        page.series()
+
+@allure.id("Тест-5")
+@allure.severity ("Blocker")
+@allure.title("Служба поддержки")
+@allure.description("Переход на страницу с сериалами")
+def test_support_service(chrome_browser):
+    with allure.step("Открытие главной страницы"):
+        chrome_browser.get("https://www.kinopoisk.ru")
+        page = BasePage(chrome_browser)
+    with allure.step("Проскролить страницу до нужного элемента"):
+        page.scroll()
+    with allure.step("Нажатие на кнопку 'Служба поддержки'"):
+        page.support_service()
+
