cbc1b0c431551ba0a20048059f06ec35d1bc0533
abdullaevaaa0801 <abdullaevaaa0801@gmail.com>
2024-10-13 11:13:26 +0300

    Правки

--- tests/test_UI.py
+++ tests/test_UI.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import allure
 import pytest
 from selenium import webdriver
@@ -47,6 +49,7 @@ def test_buy_ticket(chrome_browser):
     with allure.step("Переход на страницу для покупки билетов в кино на выбранную дату"):
         page.buy_ticket()
 
+
 @allure.id("Тест-4")
 @allure.severity ("Critical")
 @allure.title("Выбор сериала")
@@ -57,6 +60,7 @@ def test_series(chrome_browser):
         page = BasePage(chrome_browser)
     with allure.step("Переход на страницу с сериалами"):
         page.series()
+        
 
 @allure.id("Тест-5")
 @allure.severity ("Blocker")
@@ -70,4 +74,5 @@ def test_support_service(chrome_browser):
         page.scroll()
     with allure.step("Нажатие на кнопку 'Служба поддержки'"):
         page.support_service()
+
 
